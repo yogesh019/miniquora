9db965eb5d1181cdb23f62888093987851975587
Yogesh Sharma <sharmay376@gmail.com>
2017-01-07 02:58:42 +0530

    Customizing admin interface

--- miniquora/account/admin.py
+++ miniquora/account/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import MyUser
 
-admin.site.register(MyUser)
+@admin.register(MyUser)
+
+class ACtiveUserAdmin(admin.ModelAdmin):
+    list_display=['first_name','last_name','is_active','is_superuser','username']
+    search_fields=['first_name','username','email']
+    '''
+    def get_queryset(self,request):
+        return self.model.objects.filter(is_superuser=True)
+    '''
+   # actions_on_bottom=True
 # Register your models here.
