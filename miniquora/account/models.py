469b28cc9ab11a2ceb9808f6c3a398ad4d8bf725
Yogesh Sharma <sharmay376@gmail.com>
2017-01-07 03:01:01 +0530

    Added otp generation methods to the account model and added an otp class

--- miniquora/account/models.py
+++ miniquora/account/models.py
@@ -1,3 +1,4 @@
+from random import randint
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -5,3 +6,50 @@ from django.contrib.auth.models import AbstractUser
 
 class MyUser(AbstractUser):
     phone=models.CharField(max_length=10,null=True)
+    profile_pic=models.ImageField(upload_to='profile_pics/',null=True,blank=True)
+
+
+def create_otp(user=None,purpose=None):
+    if not user:
+        raise ValueError("Invalid Arguments");
+    choices=[]
+    for choice_purpose,verbose in UserOTP.OTP_PURPOSE_CHOICES:
+        choices.append(choice_purpose)
+    if not purpose in choices:
+        raise ValueError('Invalid Arguments');
+    if UserOTP.objects.filter(user=user,purpose=purpose).exists():
+        old_otp=UserOTP.objects.get(user=user,purpose=purpose)
+        old_otp.delete()
+    otp=randint(1000,9999)
+    otp_object=UserOTP.objects.create(user=user,purpose=purpose,otp=otp)
+    return otp;
+        
+
+
+def get_valid_otp_object(user=None,otp=None,purpose=None):
+    if not user:
+        raise ValueError("Invalid Arguments");
+    choices=[]
+    for choice_purpose,verbose in UserOTP.OTP_PURPOSE_CHOICES:
+        choices.append(choice_purpose)
+    if not purpose in choices:
+        raise ValueError('Invalid Arguments');
+    try:
+        otp_object=UserOTP.objects.get(user=user,purpose=purpose,otp=otp)
+        return otp_object
+    except UserOTP.DoesNotExist:
+        return None;
+
+    
+class UserOTP(models.Model):
+    
+    OTP_PURPOSE_CHOICES=(
+            ('FP','Forgot Password'),
+            ('AA','Activate Account'),
+    )
+    user=models.ForeignKey(MyUser)
+    otp=models.CharField(max_length=4)
+    purpose=models.CharField(max_length=2,choices=OTP_PURPOSE_CHOICES)
+    created_on=models.DateTimeField(auto_now_add=True)
+    class Meta:
+        unique_together=['user','purpose']
